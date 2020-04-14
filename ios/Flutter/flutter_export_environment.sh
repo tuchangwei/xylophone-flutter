be9b6781452eaf236215aadcda81340921ba580a
Changwei Tu <changweitu@gmail.com>
2020-04-14 09:24:48 +0800

    done

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/tu/Developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/tu/Documents/Flutter/Code/xylophone-flutter"
+export "FLUTTER_TARGET=/Users/tu/Documents/Flutter/Code/xylophone-flutter/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/tu/Developer/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
 
 void main() => runApp(XylophoneApp());
 
@@ -8,9 +9,51 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Content(),
         ),
       ),
+    );
+  }
+}
+
+class Content extends StatefulWidget {
+  @override
+  _ContentState createState() => _ContentState();
+}
+
+class _ContentState extends State<Content> {
+  List<Expanded> createExpanded() {
+    const List<Color> colors = [
+      Colors.red,
+      Colors.orange,
+      Colors.yellow,
+      Colors.green,
+      Colors.teal,
+      Colors.blue,
+      Colors.purple
+    ];
+    List<Expanded> widgets = new List();
+    for (var i = 0; i < colors.length; i++) {
+      var color = colors[i];
+      Expanded widget = Expanded(
+        child: FlatButton(
+          onPressed: () {
+            final cachePlayer = AudioCache();
+            cachePlayer.play('note${i + 1}.wav');
+          },
+          color: color,
+        ),
+      );
+      widgets.add(widget);
+    }
+    return widgets;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: createExpanded(),
     );
   }
 }
